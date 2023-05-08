6da583121c4bc02189a89437c20e89e9ea285b0b
aruunabh <132893799+aruunabh@users.noreply.github.com>
2023-05-08 15:34:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+WebGLSampler.to("#page1",{
+  scrollTrigger:{
+    trigger:`#page1`,
+    start:`top top`,
+    end:`bottom top`,
+    pin:true,
+    scroller:`#main`
+  }
+})
+WebGLSampler.to("#page2",{
+  scrollTrigger:{
+    trigger:`#page1`,
+    start:`top top`,
+    end:`bottom top`,
+    pin:true,
+    scroller:`#main`
+  }
+})
